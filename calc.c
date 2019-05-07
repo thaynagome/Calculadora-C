2af94492580b1e8846c6ad2633f1bad8bb39f2b4
Thayna Gomes <tluzsantos@hotmail.com>
2019-05-07 10:06:17 -0300

    operações

--- calc.c
+++ calc.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main (void){
-    float x,y; //valores utilizados no código
-    int op; //variavel responsavel em saber tipo do calculo
+    float x=0, y=0, resultado=0; //valores utilizados no código
+    int op=0; //variavel responsavel em saber tipo do calculo
 
     do{
         printf("\n\t1 - SOMA \n\t2 SUBTRACAO \n\t3 - MULTIPLICACAO \n\t4 - DIVISAO"); //apresenta para o usuario
@@ -11,5 +11,24 @@ int main (void){
         scanf("%f", &x);
         printf("\nDigite o outro numero: ");
         scanf("%f", &y);
+
+        switch(op){
+            case 1:
+                resultado x+y;
+                break;
+            case 2:
+                resultado x-y;
+                break;
+            case 3:
+                resultado x*y;
+                break;
+            case 4:
+                resultado x/y;
+                break;
+            default:
+                printf("\nDigite uma opcao valida");
+                break;
+            
+        }
     }while();
 }
